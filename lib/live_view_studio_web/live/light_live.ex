709a935c670306a2320a85cd67e80b6c0391c074
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-05-05 16:49:06 -0400

    Update light_live.ex

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -17,13 +17,41 @@ defmodule LiveViewStudioWeb.LightLive do
     </span>
     </div>
 
-    <button>
+    <button  phx-click="off" >
     <img src="images/light-off.svg">
     </button>
 
-    <button>
-    <img src="images/light-on.svg">
+    <button  phx-click="down" >
+    <img src="images/down.svg">
+    </button>
+
+    <button  phx-click="up" >
+    <img src="images/up.svg">
+    </button>
+
+    <button phx-click="on">
+    <img  src="images/light-on.svg">
     </button>
     """
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket = update(socket, :brightness, &(&1 - 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket = update(socket, :brightness, &(&1 + 10))
+    {:noreply, socket}
   end
 end
