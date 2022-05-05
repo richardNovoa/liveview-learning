a4f8094c5d6dd17ad6237d8c2590d5486ce88ec4
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-05-05 16:11:58 -0400

    reset

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,9 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, :brightness, 10)
+    IO.inspect(socket)
+    {:ok, socket}
+  end
+end
