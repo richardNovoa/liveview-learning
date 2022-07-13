4b7b8d39fd0dba79c62a4def68ddfd522e95ff14
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-07-13 13:08:27 -0400

    replace brightness buttons with dynamicslider

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -16,42 +16,17 @@ defmodule LiveViewStudioWeb.LightLive do
     <%= @brightness %>%
     </span>
     </div>
+    <form phx-change="update">
+    <input type="range" min="0" max="100" name="brightness" value="<%= @brightness%>" />
+    </form>
 
-    <button  phx-click="off" >
-    <img src="images/light-off.svg">
-    </button>
 
-    <button  phx-click="down" >
-    <img src="images/down.svg">
-    </button>
-
-    <button  phx-click="up" >
-    <img src="images/up.svg">
-    </button>
-
-    <button phx-click="on">
-    <img  src="images/light-on.svg">
-    </button>
     """
   end
 
-  def handle_event("on", _, socket) do
-    socket = assign(socket, :brightness, 100)
-    {:noreply, socket}
-  end
-
-  def handle_event("off", _, socket) do
-    socket = assign(socket, :brightness, 0)
-    {:noreply, socket}
-  end
-
-  def handle_event("down", _, socket) do
-    socket = update(socket, :brightness, &(&1 - 10))
-    {:noreply, socket}
-  end
-
-  def handle_event("up", _, socket) do
-    socket = update(socket, :brightness, &(&1 + 10))
+  def handle_event("update", %{"brightness" => brightness}, socket) do
+    brightness = String.to_integer(brightness)
+    socket = assign(socket, brightness: brightness)
     {:noreply, socket}
   end
 end
