0bc03b3837eb8a6d888aec694eaca7409c7aaa89
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2022-07-19 13:55:12 -0400

    countdown timer handle_info

--- lib/live_view_studio_web/live/sales_dashboard_live.ex
+++ lib/live_view_studio_web/live/sales_dashboard_live.ex
@@ -8,6 +8,8 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
       :timer.send_interval(1000, self(), :tick)
     end
 
+    expiration_time = Timex.shift(Timex.now(), hours: 1)
+
     socket = assign_stats(socket)
 
     {:ok, socket}
@@ -15,6 +17,7 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
 
   def render(assigns) do
     ~L"""
+
     <h1>Sales Dashboard</h1>
     <div id="dashboard">
     <div class="stats">
@@ -67,5 +70,21 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
       sales_amount: Sales.sales_amount(),
       satisfaction: Sales.satisfaction()
     )
+  end
+
+  defp time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
+  end
+
+  def handle_info(:tick, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket = assign(socket, time_remaining: time_remaining(expiration_time))
+    {:noreply, socket}
   end
 end
